2d5bfc836645366089768857afe0c6f280d9654e
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-08-18 17:26:31 -0300

    adds some libs

--- cuidar_admin/src/App.js
+++ cuidar_admin/src/App.js
@@ -1,7 +1,30 @@
-import Home from './pages/login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+import Login from './pages/login';
+
+const queryClient = new QueryClient();
+
+toast.configure();
 function App() {
-  return <Home />;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <main>
+            <Switch>
+              <Route exact path="/">
+                <Login />
+              </Route>
+            </Switch>
+          </main>
+        </Router>
+      </QueryClientProvider>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
