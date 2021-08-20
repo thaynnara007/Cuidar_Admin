32720fef951eb3ab941b22391b5a07d8d6dd109f
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-08-20 16:03:15 -0300

    adds profile at navbar

--- cuidar_admin/src/components/navbar/index.js
+++ cuidar_admin/src/components/navbar/index.js
@@ -5,17 +5,30 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+
 import MenuIcon from '../icons/iconMenu';
+import ProfileIcon from '../icons/iconProfile';
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
+    '& header.MuiAppBar-root': {
+      backgroundColor: '#3F72AF',
+    },
   },
   menuButton: {
     marginRight: '20px',
   },
   title: {
     flexGrow: 1,
+    color: '#F9F7F7',
+  },
+  button: {
+    color: '#F9F7F7',
+  },
+  avatar: {
+    backgroundColor: '#F9F7F7',
   },
 });
 
@@ -29,10 +42,17 @@ function Navbar() {
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon color="#F9F7F7" />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h5" className={classes.title}>
             Cuidar
           </Typography>
-          <Button color="inherit">SAIR</Button>
+          <IconButton color="inherit" aria-label="profile">
+            <Avatar className={classes.avatar}>
+              <ProfileIcon color="#112D4E" />
+            </Avatar>
+          </IconButton>
+          <Button color="inherit" className={classes.button}>
+            SAIR
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
